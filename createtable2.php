8247b0fde07afaf44c1f12d84b161ed106d9e001
rivai izari <rivaiizari@gmail.com>
2019-05-21 17:00:06 +0700

    Update createtable2.php

--- createtable2.php
+++ createtable2.php
@@ -3,7 +3,7 @@
 	$db = "web3";
 	// PHP Data Objects(PDO) Sample Code:
 	try {
-	    $conn = new PDO("sqlsrv:server = tcp:dbizariazur.database.windows.net,1433; Database = web3", "dbizariazur", $db);
+	    $conn = new PDO("sqlsrv:server = tcp:dbizariazur.database.windows.net,1433; Database = web3", "dbizariazur", "14051995Dp");
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (PDOException $e) {
@@ -12,7 +12,7 @@
 	}
 
 	// SQL Server Extension Sample Code:
-	$connectionInfo = array("UID" => "dbizariazur@dbizariazur", "pwd" => $db, "Database" => "web3", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$connectionInfo = array("UID" => "dbizariazur@dbizariazur", "pwd" => "14051995Dp", "Database" => "web3", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 	$serverName = "tcp:dbizariazur.database.windows.net,1433";
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
